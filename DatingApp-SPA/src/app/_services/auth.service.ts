3b78b53157b163b6eec96a05d388bfa8a324d9c4
Ahmed ElAbbasy <ahmedelabbasy74@gmail.com>
2019-06-30 04:40:20 +0200

    end of sec 4

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -11,7 +11,7 @@ export class AuthService {
 constructor(private http: HttpClient) {}
 
 login(model: any) {
-  return this.http.post(this.baseUrl, 'login', model).pipe(
+  return this.http.post(this.baseUrl + 'login', model).pipe(
     map((response: any) => {
         const user = response;
         if (user) {
@@ -19,6 +19,10 @@ login(model: any) {
         }
     })
   );
+}
+
+register(model: any) {
+  return this.http.post(this.baseUrl + 'register' , model);
 }
 
 }
