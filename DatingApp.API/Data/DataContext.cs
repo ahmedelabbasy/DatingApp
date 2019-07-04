3c98c538459793af827b201527d4fcde319bbd7c
Ahmed ElAbbasy <ahmedelabbasy74@gmail.com>
2019-07-04 04:32:40 +0200

    End Of sec 8

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -6,10 +6,9 @@ namespace DatingApp.API.Data
     public class DataContext : DbContext 
     {
         public DataContext(DbContextOptions<DataContext>options) : base (options) {}
-        
         public DbSet<Value> values { get; set; } 
-
         public DbSet<User> Users { get; set; }
+        public DbSet<Photo> Photos { get; set; }
         
     }
 }
